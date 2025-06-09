6d571753b88338420804485d84a1fae5088ae036
Suvajit2640 <suvajit2003saha@gmail.com>
2025-06-10 01:37:05 +0530

    feat:Registration done

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,10 +1,16 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Registration from "./pages/Registration";
+
 function App() {
   return (
     <>
-      <Navbar />
-      <Hero/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/register" element={<Registration />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -49,7 +49,7 @@ export default function () {
             <li>
               <a
                 href="#"
-                class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                class="block py-2 px-3 text-white  rounded-sm md:bg-transparent  md:p-0 dark:text-white "
                 aria-current="page"
               >
                 Home
@@ -69,6 +69,14 @@ export default function () {
                 class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 How it works
+              </a>
+            </li>
+            <li>
+              <a
+                href="register"
+                class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Register
               </a>
             </li>
           </ul>
