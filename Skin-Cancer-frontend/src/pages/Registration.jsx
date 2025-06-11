b8dde980613ccd40554ecabda1f85cca6a44d294
Rupsha Bhattacharyya <ru9433280682@gmail.com>
2025-06-11 16:27:11 +0530

    SignUp Color

--- Skin-Cancer-frontend/src/pages/Registration.jsx
+++ Skin-Cancer-frontend/src/pages/Registration.jsx
@@ -69,7 +69,7 @@ export default function Registration() {
                   </p>
                 </div>
                 <div>
-                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
                     Your email
                   </label>
                   <input
@@ -136,7 +136,7 @@ export default function Registration() {
                 </div>
                 <button
                   type="submit"
-                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  className="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
                   Sign Up
                 </button>
